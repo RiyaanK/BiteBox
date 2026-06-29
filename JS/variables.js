4d5ae0afceb687139ae0b1f8147ce23d4094bfeb
riyaan <riyaan.scorpio@gmail.com>
2026-06-29 20:34:15 +0530

    second commit

---
+++ JS/variables.js
@@ -0,0 +1,21 @@
+console.log("Welcome");
+
+const id=123;
+console.log("id:",id);
+ id =567;
+console.log("id:",id);
+
+
+
+// variables using var keyword
+//  - can be reassigned , redeclared
+
+// variables using let keyword
+//  can be reassigned , but not declared
+
+// variables using const keyword
+// 
+
+
+
+
